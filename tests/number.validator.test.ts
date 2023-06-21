5c624ceb9555602b45f123afc5fdd19d2a5fc982
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2023-06-21 23:22:26 +0300

    Add number validator test

---
+++ tests/number.validator.test.ts
@@ -0,0 +1,23 @@
+import { IValidator } from '../src/validators/IValidator';
+import { NumberValidator } from '../src/validators/Number.Validator';
+describe('Number Validator Test', () => {
+    let numberValidator: IValidator;
+
+    beforeEach(() => {
+        numberValidator = new NumberValidator(10, 'number-input');
+    });
+
+    test('Value should be jane', () => {
+        expect(numberValidator.value).toEqual(10);
+    });
+
+    test('Validator Name should be number-input', () => {
+        expect(numberValidator.validatorName).toEqual('number-input');
+    });
+
+    test('Validation result should not valid', () => {
+        numberValidator.value = '10';
+        const result = numberValidator.validate();
+        expect(result.isValid).toEqual(false);
+    });
+});
