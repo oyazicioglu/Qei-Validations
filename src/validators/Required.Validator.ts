a6427c99d210e8fba2b476fc02213edcb0c7d090
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2023-06-21 23:00:46 +0300

    Add required validator

---
+++ src/validators/Required.Validator.ts
@@ -0,0 +1,20 @@
+import { ValidatorResult } from './Validator.Result';
+import { IValidator, ValidatorValueType } from './IValidator';
+import { Validator } from './Validator';
+
+export class RequiredValidator extends Validator implements IValidator {
+    constructor(value: ValidatorValueType, validatorName: string) {
+        super(value, validatorName);
+    }
+
+    validate(): ValidatorResult {
+        const valid = this.value && this.value !== '' && this.value !== 0;
+        return {
+            isValid: valid,
+            error: {
+                message: `${this.validatorName} is required.`,
+                validatorName: this.validatorName,
+            },
+        };
+    }
+}
