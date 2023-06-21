feccb48bfbb12b32de13c98c87cd07c7d55c3e00
oyazicioglu <oyazicioglu@hotmail.com>
2023-06-21 21:12:53 +0300

    Renew Library

--- build/MaxValueValidation.d.ts
+++
@@ -1,9 +0,0 @@
-import { IValidation, ValidationError } from './IValidation';
-export declare class MaxValueValidation implements IValidation {
-    max: number;
-    value: number;
-    name: string;
-    constructor(max: number, value: number, name: string);
-    validate(): ValidationError;
-    getValue(): number;
-}
--- build/RequiredValidation.d.ts
+++
@@ -1,7 +0,0 @@
-import { IValidation, ValidationError } from './IValidation';
-export declare class RequiredValidation implements IValidation {
-    value: string;
-    name: string;
-    constructor(value: string, name: string);
-    validate(): ValidationError;
-}
