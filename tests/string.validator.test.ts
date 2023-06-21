5453d9ded3a27474391853bd254e72e8352360b1
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2023-06-21 22:35:52 +0300

    Add StringValidator test

---
+++ tests/string.validator.test.ts
@@ -0,0 +1,17 @@
+import { StringValidator } from '../src/validators/StringValidator';
+describe('String Validator Test', () => {
+    const stringValidator = new StringValidator('jane', 'username');
+    test('Value should be jane', () => {
+        expect(stringValidator.value).toEqual('jane');
+    });
+
+    test('Validator Name should be username', () => {
+        expect(stringValidator.validatorName).toEqual('username');
+    });
+
+    test('Validation result should not valid', () => {
+        stringValidator.value = 13;
+        const result = stringValidator.validate();
+        expect(result.isValid).toEqual(false);
+    });
+});
