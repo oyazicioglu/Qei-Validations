c55bcb4917eb0d8cb4eb8e0622cbd1ab139c4703
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2023-06-21 22:35:42 +0300

    Implement String Validator validate method

--- src/validators/StringValidator.ts
+++ src/validators/StringValidator.ts
@@ -1,24 +1,36 @@
-import { ValidatorResult } from "./Validator.Result";
-import { IValidator, ValidatorValueType } from "./IValidator";
+import { ValidatorResult } from './Validator.Result';
+import { IValidator, ValidatorValueType } from './IValidator';
 
 export class StringValidator implements IValidator {
-    
-    private _value : ValidatorValueType;
-    public get value() : ValidatorValueType {
+    private _value: ValidatorValueType;
+    public get value(): ValidatorValueType {
         return this._value;
     }
-    public set value(v : ValidatorValueType) {
+    public set value(v: ValidatorValueType) {
         this._value = v;
-
     }
-    
-    constructor(value: ValidatorValueType) {
+
+    private _validatorName: string;
+    public get validatorName(): string {
+        return this._validatorName;
+    }
+    public set validatorName(v: string) {
+        this._validatorName = v;
+    }
+
+    constructor(value: ValidatorValueType, validatorName: string) {
         this._value = value;
+        this._validatorName = validatorName;
     }
 
     validate(): ValidatorResult {
+        const valid = this.value && typeof this.value === 'string';
         return {
-            isValid: true,
-        }
+            isValid: valid,
+            error: {
+                message: 'Value is not type of String',
+                validatorName: this.validatorName,
+            },
+        };
     }
 }
