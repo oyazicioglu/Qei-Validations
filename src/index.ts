aa98a48dc6c74949c228ad41a767492b70114ce1
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2023-06-21 23:03:28 +0300

    Export validators

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import { IValidator, ValidatorValueType } from "./validators/IValidator";
-import { IValidation, ValidationResult } from "./validation/IValidation";
-import { Validation } from "./validation/Validation";
-import { ValidatorError } from "./validation/Validation.Error";
-import { ValidatorResult } from "./validators/Validator.Result";
-import { StringValidator } from "./validators/StringValidator";
+import { IValidator, ValidatorValueType } from './validators/IValidator';
+import { IValidation, ValidationResult } from './validation/IValidation';
+import { Validation } from './validation/Validation';
+import { ValidatorError } from './validation/Validation.Error';
+import { ValidatorResult } from './validators/Validator.Result';
+import { StringValidator } from './validators/StringValidator';
+import { RequiredValidator } from './validators/Required.Validator';
+import { MaxLengthValidator } from './validators/MaxLength.Validator';
+import { MinLengthValidator } from './validators/MinLength.Validator';
+import { MaxValueValidator } from './validators/MaxValue.Validator';
+import { MinValueValidator } from './validators/MinValue.Validator';
 
 export {
     IValidator,
@@ -13,5 +18,10 @@ export {
     StringValidator,
     Validation,
     ValidatorError,
-    ValidatorResult
-}
+    ValidatorResult,
+    RequiredValidator,
+    MaxLengthValidator,
+    MinLengthValidator,
+    MaxValueValidator,
+    MinValueValidator,
+};
